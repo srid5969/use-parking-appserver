d79eb4ab42758a1996ab6529cbc0ecadad2c6ded
Sridhar <156986808+sridhar5969@users.noreply.github.com>
2025-03-27 21:23:37 +0530

    Add configuration management and improve application bootstrap logging

---
+++ src/configs/app-configs.ts
@@ -0,0 +1,19 @@
+export const config = {
+  port: parseInt(process.env.PORT || '8080', 10),
+
+  // #  database configurations
+  dbURI: process.env.MONGODB_CONNECTION_STRING,
+  database: process.env.DATABASE,
+
+  // password pepper
+  pepper: process.env.PEPPER_STRING,
+
+  // auth token configurations
+  jwtExpiry: process.env.JWT_EXPIRY,
+  JwtSecret: process.env.JWT_SECRET,
+  refreshJwtSecret: process.env.REFRESH_JWT_SECRET,
+  refreshTokenExpiry: process.env.REFRESH_JWT_EXPIRY,
+  AdminBaseUrl: process.env.ADMIN_BASE_URL || 'http://localhost:3000',
+};
+
+// export type EnvironmentConfigType = ReturnType<typeof config>;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from './configs';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(config.port);
+  Logger.log(`Application running on port ${config.port}`);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  Logger.error('Error during application bootstrap', error);
+});
