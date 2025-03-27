aaec199d26f7d1ca680012c84ea3dac801505764
Sridhar <156986808+sridhar5969@users.noreply.github.com>
2025-03-27 22:26:22 +0530

    Fix syntax error in app-messages and add AdminController to UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { LoginAuthService } from './services/login-auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/users.schema';
+import { AdminController } from './controllers/admin.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   providers: [LoginAuthService],
+  controllers: [AdminController],
 })
 export class UsersModule {}
