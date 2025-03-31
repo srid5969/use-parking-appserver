c8516f1fff308616ec2252d8dd0c139c784ab4d2
Sridhar <156986808+sridhar5969@users.noreply.github.com>
2025-03-31 10:45:12 +0530

    Add email and phone existence check during admin creation

--- src/users/services/admin/admin-managements.service.ts
+++ src/users/services/admin/admin-managements.service.ts
@@ -21,6 +21,7 @@ export class AdminManagementService {
     admin.createdBy =
       typeof creator === 'string' ? new Types.ObjectId(creator) : creator;
     admin.status = UserStatus.ACTIVE;
+    await this.userService.checkIfEmailOrPhoneAlreadyExists(admin.email, admin.phone);
     const createdAdmin = await admin.save();
     return createdAdmin;
   }
